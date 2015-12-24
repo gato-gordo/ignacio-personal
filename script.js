1abe663ed920317643f01f6ec81ed4d1a800b027
Ignacio Prado <ignacio@gatogordoweb.com>
2015-12-24 12:41:35 -0500

    Update site for January-2016

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
 	$('.button').click(function(event){
 		event.preventDefault();
-		event.stopPropagation();
-		//console.log($(this).parent().find('a'));
+		event.stopPropagation()
 		$(this).parent().find('a')[0].click();
 	});
 });
